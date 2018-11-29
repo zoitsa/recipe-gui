73e6524abbce74b1b8844d5673c4f5800b3f221c
Zoe C Koulouris <zck0430@gmail.com>
2018-11-28 20:34:43 -0500

    added apiUrl to apiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+
+  private apiURL = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
