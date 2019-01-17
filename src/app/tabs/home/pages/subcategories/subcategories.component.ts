55aef21fa9995dddced997a181f021937faac2f5
Zoe Koulouris <zoe@upstate.agency>
2019-01-17 16:57:04 -0500

    added recipe page with routing and displaying recipes on the page

--- src/app/tabs/home/pages/subcategories/subcategories.component.ts
+++ src/app/tabs/home/pages/subcategories/subcategories.component.ts
@@ -5,6 +5,8 @@ import { CMSActions } from '../../../../services/dispatcher.service';
 import { CategoryActions } from '../../../../actions/categories.actions';
 import { Observable } from 'rxjs/Observable';
 
+import { RecipeActions } from '../../../../actions/recipes.actions';
+import { SubCategoryActions } from '../../../../actions/subCategories.actions';
 
 @Component({
   selector: 'app-subcategories',
@@ -13,18 +15,28 @@ import { Observable } from 'rxjs/Observable';
 })
 export class SubcategoriesComponent implements OnInit {
   subCategories$: Observable<any>;
+  // recipes$: Observable<any>;
 
   constructor(
     private store: Store<fromRoot.State>,
     private actions$: CMSActions,
   ) {
     this.subCategories$ = this.store.select(fromRoot.selectSelectedCategory);
+    // this.recipes$ = this.store.select(fromRoot.selectAllRecipes);
    }
+
+   selectSubCategory(subCategoryId) {
+    this.store.dispatch(new SubCategoryActions.Select(subCategoryId));
+    this.store.dispatch(new RecipeActions.Get(subCategoryId));
+  }
 
   ngOnInit() {
     this.subCategories$.subscribe(data => {
       console.log(data);
     });
+    // this.recipes$.subscribe(data => {
+    //   console.log(data);
+    // });
   }
 
 }
