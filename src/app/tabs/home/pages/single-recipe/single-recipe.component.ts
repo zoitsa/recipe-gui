b880722cc312715acc27cb983ea56b1999274ad0
Zoe Koulouris <zoe@upstate.agency>
2019-04-01 12:04:38 -0400

    feat(steps): added back missing code for the itemSelected function

--- src/app/tabs/home/pages/single-recipe/single-recipe.component.ts
+++ src/app/tabs/home/pages/single-recipe/single-recipe.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from '../../../../reducers';
 import { CMSActions } from '../../../../services/dispatcher.service';
 import { Observable } from 'rxjs/Observable';
 import { RecipeActions } from '../../../../actions/recipes.actions';
+import { ListViewEventData } from 'nativescript-ui-listview';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-single-recipe',
   templateUrl: './single-recipe.component.html',
   styleUrls: ['./single-recipe.component.scss']
 })
-export class SingleRecipeComponent implements OnInit {
+export class SingleRecipeComponent implements OnInit, OnDestroy {
   singleRecipe$: Observable<any>;
   recipeId$;
   recipeSteps$;
@@ -18,7 +22,8 @@ export class SingleRecipeComponent implements OnInit {
   checkedOption = false;
   selectedIndex = 0;
   items: Array<any>;
-  segmentedBar;
+  private unsubscribe: Subject<void> = new Subject();
+
 
   constructor(
     private store: Store<fromRoot.State>,
@@ -28,26 +33,38 @@ export class SingleRecipeComponent implements OnInit {
     this.singleRecipe$ = this.store.select(fromRoot.selectSelectedRecipe);
   }
 
+  // -- rework this to work for the ingredient list
+  // checkedChange(stepsCheck, i) {
+  //   console.log('stepsCheck');
+  //   console.log(stepsCheck);
+  //   console.log(i);
+  //   // console.log(stepsCheck.checked);
+  //   // console.log(stepsCheck.toggle);
+  //   // console.log(this.singleRecipe.steps[i]);
+  //   this.singleRecipe$.subscribe(data => {
+  //     this.singleRecipe = data;
+  //   });
+  //   this.store.dispatch(new RecipeActions.ToggleStep(this.singleRecipe.steps[i]));
+  // }
 
-  checkedChange(stepsCheck, i) {
-    console.log('stepsCheck');
-    console.log(stepsCheck);
-    console.log(i);
-    // console.log(stepsCheck.checked);
-    // console.log(stepsCheck.toggle);
-    // console.log(this.singleRecipe.steps[i]);
-    this.singleRecipe$.subscribe(data => {
+  public itemSelected(args: ListViewEventData) {
+    this.singleRecipe$.pipe(
+      takeUntil(this.unsubscribe)
+    )
+    .subscribe(data => {
       this.singleRecipe = data;
     });
-    this.store.dispatch(new RecipeActions.ToggleStep(this.singleRecipe.steps[i]));
-  }
+    this.store.dispatch(new RecipeActions.ToggleStep(this.singleRecipe.steps[args.index]));
+}
+
 
   ngOnInit() {
 
-    // this.recipeId$.subscribe(data => {
-    //   console.log('single-recipe');
-    //   console.log(data);
-    // });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
