d9b93c06f4e37d05c1c78afebaa8b013e263588c
Kent Walker <kent@upstate.agency>
2019-04-02 16:38:23 -0400

    feat(app): implemented image picker and camera controls on 'Create Recipe' form

--- src/app/tabs/create-recipe/components/create-form/create-form.component.ts
+++ src/app/tabs/create-recipe/components/create-form/create-form.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, ViewChild, ElementRef, Input, OnChanges, SimpleChang
 import { SelectedIndexChangedEventData } from 'nativescript-drop-down';
 import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 import * as imagepicker from "nativescript-imagepicker";
+import {ImageSource, fromFile, fromResource, fromBase64} from "tns-core-modules/image-source";
+import {Folder, path, knownFolders} from "tns-core-modules/file-system";
+import * as camera from "nativescript-camera";
+
+// import { Image } from "tns-core-modules/ui/image";
 // import { Page } from 'ui/page';
 
 
@@ -20,6 +25,13 @@ export class CreateFormComponent implements OnInit, OnChanges {
   @ViewChild('category') categoryDropDown: ElementRef;
   @ViewChild('type') typeDropDown: ElementRef;
   @Output() category: EventEmitter<any> = new EventEmitter();
+  imageAssets = [];
+  imageUris = [];
+  captureAssets = [];
+  captureUris = [];
+  imageSrc: any;
+  imageIndex = 1;
+  captureIndex = 1;
 
   public selectedIndex = 1;
     public items: Array<string>;
@@ -46,7 +58,8 @@ export class CreateFormComponent implements OnInit, OnChanges {
       name: new FormControl(null, {updateOn: 'blur'}),
       description: new FormControl(null, {updateOn: 'blur'}),
       ingredients: new FormArray([new FormControl(null)]),
-      steps: new FormArray([new FormControl(null)])
+      steps: new FormArray([new FormControl(null)]),
+      imageNames: new FormArray([])
     });  
   }
 
@@ -77,5 +90,98 @@ export class CreateFormComponent implements OnInit, OnChanges {
    
   }
 
+  onSelectImage() {
+    let that = this;    
+
+    let context = imagepicker.create({
+      mode: "single" // use "multiple" for multiple selection
+    });
+
+    context
+    .authorize()
+    .then(function() {       
+        that.imageSrc = null;
+        return context.present();
+    })
+    .then(function(selection) {
+        selection.forEach(function(selected) {
+              // process the selected image
+          
+            const source = new ImageSource();
+            source.fromAsset(selected)
+            .then((imageSource: ImageSource) => {
+            const folderPath: string = knownFolders.documents().path;
+            const fileName = `test${that.imageIndex}.png`;
+            const filePath = path.join(folderPath, fileName);
+            console.log(filePath);
+            that.imageUris.push(filePath);
+            const saved: boolean = imageSource.saveToFile(filePath, "png");
+            if (saved) {
+                console.log("Image saved successfully!");         
+                that.imageAssets.push(fileName);                        
+                for(let asset of that.imageAssets){
+                  console.log(asset);
+                }
+                that.imageIndex++;
+            }           
+          
+              
+          })
+        })
+    }).catch(function (e) {
+        // process error
+        console.log(e);
+    });
+
+    
+  }
+
+  onCaptureImage() {
+        let that = this;
+          if(camera.isAvailable()){
+              camera.requestPermissions().then(
+                  // function success() {
+                      () => {
+                          camera.takePicture()
+                          .then((imageAsset) => {
+                              const source = new ImageSource();
+                              // source.fromAsset(imageAsset);
+                              
+                              source.fromAsset(imageAsset).then((imageSource: ImageSource) => {
+                                const folderPath: string = knownFolders.documents().path;
+                                const fileName = `capture${that.captureIndex}.png`;
+                                const filePath = path.join(folderPath, fileName);
+                                that.captureUris.push(filePath);
+                                const saved: boolean = imageSource.saveToFile(filePath, "png"); 
+                                if (saved) {
+                                  console.log("Image saved successfully!");         
+                                  // that.captureAssets.push(fileName);                        
+                                  // for(let asset of that.imageAssets){
+                                  //   console.log(asset);
+                                  // }
+                                  that.captureIndex++;
+                                }  
+                              }, (err) => {
+                                      console.log("Error -> " + err.message);
+                              });
+
+
+                           })
+                          .catch((err) => {
+                              console.log("Error -> " + err.message);
+                          });
+                      }
+
+                  // },
+                  // function failure() {
+                  //     dialogs.alert("Permission Denied").then(()=> {
+                  //         console.log("Dialog closed!");
+                  //     });
+                  // }
+
+              )
+      }
+  }
+  
 
 }
